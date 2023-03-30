8127d27aa03c587cfd79165fd12cab963ee4a168
Denis Kulik <kulikdenis1996x@gmail.com>
2023-03-30 19:53:35 +0700

    update markup App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,50 @@
 import './App.scss';
 import logo from './img/logo.png';
+import background from './img/background.jpg';
+import user from './img/user.jpg';
+
 
 const App = () => {
     return (
         <div className="App">
-            <header>
-                <img src={logo} alt="logotype" />
+            <header className="header">
+                <img className="logo-img" src={logo} alt="logotype" />
             </header>
-            <nav>
-                <ul>
-                    <li>Profile</li>
-                    <li>Messages</li>
-                    <li>News</li>
-                    <li>Music</li>
-                    <li>Settings</li>
+            <nav className="sidebar">
+                <ul className="nav">
+                    <li className="navItem"><a href="#">Profile</a></li>
+                    <li className="navItem"><a href="#">Messages</a></li>
+                    <li className="navItem"><a href="#">News</a></li>
+                    <li className="navItem"><a href="#">Music</a></li>
+                    <li className="navItem"><a href="#">Settings</a></li>
                 </ul>
             </nav>
-            <main>
-                Main content
+            <main className="content">
+                <div>
+                    <div>
+                        <img className="backgroundImage" src={background}
+                             alt="background" />
+                    </div>
+                    <div className="userInfo">
+                        <img className="userImg" src={user} alt="user" />
+                        <h2 className="username">Martin S.</h2>
+                    </div>
+                    <div>
+                        <h3>My posts</h3>
+                        <div>
+                            New post
+                        </div>
+                        <div>
+                            <div>
+                                post 1
+                            </div>
+                            <div>
+                                post 2
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
             </main>
         </div>
     );
