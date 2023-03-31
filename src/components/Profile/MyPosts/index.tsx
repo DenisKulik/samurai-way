4c316320fba4d14408d9b5114c0229d789213b31
Denis Kulik <kulikdenis1996x@gmail.com>
2023-03-31 16:54:17 +0700

    add MyPosts styles

--- src/components/Profile/MyPosts/index.tsx
+++ src/components/Profile/MyPosts/index.tsx
@@ -1,18 +1,21 @@
 import styles from './MyPosts.module.scss';
+import user from '../../../img/user.jpg';
 
 const MyPosts = () => {
     return (
         <div className={styles.container}>
-            <h3>My posts</h3>
-            <div>
-                New post
+            <h3 className={styles.heading}>My posts</h3>
+            <div className={styles.postBox}>
+                <textarea className={styles.postField}
+                          placeholder="Enter text"></textarea>
+                <button className={styles.redBtn}>Add post</button>
             </div>
-            <div>
+            <div className={styles.posts}>
                 <div className={styles.item}>
-                    post 1
-                </div>
-                <div>
-                    post 2
+                    <img className={styles.userImg} src={user}
+                         alt="user" />
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                       Accusantium culpa debitis doloribus</p>
                 </div>
             </div>
         </div>
