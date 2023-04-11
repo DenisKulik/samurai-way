e92241067bc701e41702431519f341f439b91d6f
Denis Kulik <kulikdenis1996x@gmail.com>
2023-04-11 19:25:01 +0700

    move data from Dialog components to arrays

--- src/components/Dialogs/index.tsx
+++ src/components/Dialogs/index.tsx
@@ -4,27 +4,51 @@ import Message from './Message';
 
 
 const Dialogs = () => {
+    const dialogsData = [
+        { id: 1, name: 'William' },
+        { id: 2, name: 'Emma' },
+        { id: 3, name: 'James' },
+        { id: 4, name: 'Addison' },
+        { id: 5, name: 'Ethan' },
+        { id: 6, name: 'Hailey' },
+    ];
+
+    const messagesData = [
+        {
+            id: 1,
+            message: 'Hey, any advice for someone starting to learn programming?'
+        },
+        {
+            id: 2,
+            message: 'Sure! Start with a beginner-friendly language like JavaScript, and practice coding every day.'
+        },
+        {
+            id: 3,
+            message: 'Got it. Any specific resources you recommend?'
+        },
+        {
+            id: 4,
+            message: 'Codecademy and Udemy have great courses, and there are tons of coding blogs and YouTube tutorials out there too.'
+        },
+    ];
+
     return (
         <div className={ styles.dialogs }>
             <div className={ styles.dialogsItems }>
-                <Dialog id={ 1 } name={ 'William' } isActive />
-                <Dialog id={ 2 } name={ 'Emma' } isActive={ false } />
-                <Dialog id={ 3 } name={ 'James' } isActive={ false } />
-                <Dialog id={ 4 } name={ 'Addison' } isActive={ false } />
-                <Dialog id={ 5 } name={ 'Ethan' } isActive={ false } />
-                <Dialog id={ 6 } name={ 'Hailey' } isActive={ false } />
+                {
+                    dialogsData.map(user => (
+                        <Dialog key={ user.id } id={ user.id }
+                                name={ user.name }
+                                isActive={ false } />
+                    ))
+                }
             </div>
             <div className={ styles.dialogsMessages }>
-                <Message
-                    message="Hey, any advice for someone starting to learn programming?" />
-                <Message
-                    message="Sure! Start with a beginner-friendly language like
-                    JavaScript, and practice coding every day." />
-                <Message
-                    message="Got it. Any specific resources you recommend?" />
-                <Message
-                    message="Codecademy and Udemy have great courses, and there are tons
-                    of coding blogs and YouTube tutorials out there too." />
+                {
+                    messagesData.map(item => (
+                        <Message key={ item.id } message={ item.message } />
+                    ))
+                }
             </div>
         </div>
     );
