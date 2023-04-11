462bb8da4b76a7456ac98107935f7ce43ccff255
Denis Kulik <kulikdenis1996x@gmail.com>
2023-04-11 19:40:03 +0700

    move data from Post components to arrays

--- src/components/Profile/MyPosts/index.tsx
+++ src/components/Profile/MyPosts/index.tsx
@@ -2,19 +2,34 @@ import styles from './MyPosts.module.scss';
 import Post from './Post';
 
 const MyPosts = () => {
+    const postsData = [
+        {
+            id: 1,
+            message: 'Have fun creating amazing things!',
+            likesCount: 3
+        },
+        {
+            id: 2,
+            message: 'JavaScript powers modern web development.',
+            likesCount: 5
+        },
+    ];
+
     return (
-        <div className={styles.container}>
-            <h3 className={styles.heading}>My posts</h3>
-            <div className={styles.postBox}>
-                <textarea className={styles.postField}
+        <div className={ styles.container }>
+            <h3 className={ styles.heading }>My posts</h3>
+            <div className={ styles.postBox }>
+                <textarea className={ styles.postField }
                           placeholder="Enter text"></textarea>
-                <button className={styles.redBtn}>Add post</button>
+                <button className={ styles.redBtn }>Add post</button>
             </div>
-            <div className={styles.posts}>
-                <Post message={'Have fun creating amazing things!'}
-                      likesCount={3} />
-                <Post message={'JavaScript powers modern web development.'}
-                      likesCount={5} />
+            <div className={ styles.posts }>
+                {
+                    postsData.map(post => (
+                        <Post key={ post.id } message={ post.message }
+                              likesCount={ post.likesCount } />
+                    ))
+                }
             </div>
         </div>
     );
