6262ff00dbf99dc89cb1df4bf1a5a641f773e1e7
Denis Kulik <kulikdenis1996x@gmail.com>
2023-04-18 20:07:57 +0700

    save textarea field value on button click

--- src/components/Profile/MyPosts/index.tsx
+++ src/components/Profile/MyPosts/index.tsx
@@ -1,6 +1,7 @@
 import styles from './MyPosts.module.scss';
 import Post from './Post';
 import { PostType } from '../../../redux/state';
+import React from 'react';
 
 type MyPostsPropsType = {
     postsData: PostType[]
@@ -9,13 +10,23 @@ type MyPostsPropsType = {
 const MyPosts = (props: MyPostsPropsType) => {
     const { postsData } = props;
 
+    const postMessageRef = React.createRef<HTMLTextAreaElement>();
+
+    const addPost = () => {
+        const postMessage = postMessageRef.current?.value;
+        alert(postMessage);
+    };
+
     return (
         <div className={ styles.container }>
             <h3 className={ styles.heading }>My posts</h3>
             <div className={ styles.postBox }>
                 <textarea className={ styles.postField }
+                          ref={ postMessageRef }
                           placeholder="Enter text"></textarea>
-                <button className={ styles.redBtn }>Add post</button>
+                <button className={ styles.redBtn } onClick={ addPost }>
+                    Add post
+                </button>
             </div>
             <div className={ styles.posts }>
                 {
