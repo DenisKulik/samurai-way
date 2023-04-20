952eb2fd69f277fd154710e34b40e33dba9fc0cf
Denis Kulik <kulikdenis1996x@gmail.com>
2023-04-20 16:35:39 +0700

    add icons for sidebar

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,5 +1,8 @@
 import { NavLink } from 'react-router-dom';
 import styles from './Sidebar.module.scss';
+import {
+    RiLayoutTopLine, RiMessage3Line, RiMusic2Line, RiSettings4Line, RiUser3Line
+} from 'react-icons/ri';
 
 const Sidebar = () => {
     return (
@@ -7,26 +10,31 @@ const Sidebar = () => {
             <ul className={ styles.nav }>
                 <li className={ styles.item }>
                     <NavLink to="/profile" activeClassName={ styles.active }>
+                        <RiUser3Line />
                         Profile
                     </NavLink>
                 </li>
                 <li className={ styles.item }>
                     <NavLink to="/dialogs" activeClassName={ styles.active }>
+                        <RiMessage3Line />
                         Messages
                     </NavLink>
                 </li>
                 <li className={ styles.item }>
                     <NavLink to="/news" activeClassName={ styles.active }>
+                        <RiLayoutTopLine />
                         News
                     </NavLink>
                 </li>
                 <li className={ styles.item }>
                     <NavLink to="/music" activeClassName={ styles.active }>
+                        <RiMusic2Line />
                         Music
                     </NavLink>
                 </li>
                 <li className={ styles.item }>
                     <NavLink to="/settings" activeClassName={ styles.active }>
+                        <RiSettings4Line />
                         Settings
                     </NavLink>
                 </li>
