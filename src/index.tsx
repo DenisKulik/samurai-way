643af95a32c76392a5f977be3d3d1fd553a6c80a
Denis Kulik <kulikdenis1996x@gmail.com>
2023-05-02 20:55:04 +0700

    use HashRouter

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,14 @@
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 import App from './App';
 import './index.scss';
 import store from './redux/state';
 
 const renderEntireTree = () => {
     ReactDOM.render(
-        <BrowserRouter>
+        <HashRouter>
             <App store={ store } dispatch={ store.dispatch.bind(store) } />
-        </BrowserRouter>,
+        </HashRouter>,
         document.getElementById('root')
     );
 };
