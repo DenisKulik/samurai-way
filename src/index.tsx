ba125257c3372db8b3d74beb85e3900e0ae06d6c
Denis Kulik <kulikdenis1996x@gmail.com>
2023-05-16 14:44:36 +0700

    remove redundant function

--- src/index.tsx
+++ src/index.tsx
@@ -2,20 +2,14 @@ import ReactDOM from 'react-dom';
 import { HashRouter } from 'react-router-dom';
 import App from './App';
 import './index.scss';
-import store, { StoreType } from './redux/reduxStore';
+import store from './redux/reduxStore';
 import { Provider } from 'react-redux';
 
-const renderEntireTree = (store: StoreType) => {
-    ReactDOM.render(
-        <HashRouter>
-            <Provider store={store}>
-                <App />
-            </Provider>
-        </HashRouter>,
-        document.getElementById('root')
-    );
-};
-
-renderEntireTree(store);
-
-store.subscribe(() => renderEntireTree(store));
+ReactDOM.render(
+    <HashRouter>
+        <Provider store={store}>
+            <App />
+        </Provider>
+    </HashRouter>,
+    document.getElementById('root')
+);
