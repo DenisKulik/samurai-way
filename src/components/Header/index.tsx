210d8f4994ac10edd051e20f90aef8a92a6c00cb
Denis Kulik <kulikdenis1996x@gmail.com>
2023-06-21 21:38:32 +0700

    move users page to developers tab in sidebar

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,17 +1,10 @@
 import styles from './Header.module.scss';
 import logo from '../../img/logo.png';
-import { NavLink } from 'react-router-dom';
-import { RiUserSearchLine } from 'react-icons/ri';
 
 const Header = () => {
     return (
         <header className={styles.header}>
             <img className={styles.logo} src={logo} alt="logotype" />
-
-            <NavLink to="/users" activeClassName={styles.active}>
-                <RiUserSearchLine />
-                Find users
-            </NavLink>
         </header>
     );
 };
--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,39 +1,46 @@
 import { NavLink } from 'react-router-dom';
 import styles from './Sidebar.module.scss';
 import {
-    RiLayoutTopLine, RiMessage3Line, RiMusic2Line, RiSettings4Line, RiUser3Line
+    RiLayoutTopLine, RiMessage3Line, RiMusic2Line, RiSettings4Line, RiUser3Line,
 } from 'react-icons/ri';
+import { MdDeveloperMode } from 'react-icons/md';
 
 const Sidebar = () => {
     return (
-        <nav className={ styles.sidebar }>
-            <ul className={ styles.nav }>
-                <li className={ styles.item }>
-                    <NavLink to="/profile" activeClassName={ styles.active }>
+        <nav className={styles.sidebar}>
+            <ul className={styles.nav}>
+                <li className={styles.item}>
+                    <NavLink to="/profile" activeClassName={styles.active}>
                         <RiUser3Line />
                         Profile
                     </NavLink>
                 </li>
-                <li className={ styles.item }>
-                    <NavLink to="/dialogs" activeClassName={ styles.active }>
+                <li className={styles.item}>
+                    <NavLink to="/dialogs" activeClassName={styles.active}>
                         <RiMessage3Line />
                         Messages
                     </NavLink>
                 </li>
-                <li className={ styles.item }>
-                    <NavLink to="/news" activeClassName={ styles.active }>
+                <li className={styles.item}>
+                    <NavLink to="/users" activeClassName={styles.active}>
+                        <MdDeveloperMode />
+                        Developers
+                    </NavLink>
+                </li>
+                <li className={styles.item}>
+                    <NavLink to="/news" activeClassName={styles.active}>
                         <RiLayoutTopLine />
                         News
                     </NavLink>
                 </li>
-                <li className={ styles.item }>
-                    <NavLink to="/music" activeClassName={ styles.active }>
+                <li className={styles.item}>
+                    <NavLink to="/music" activeClassName={styles.active}>
                         <RiMusic2Line />
                         Music
                     </NavLink>
                 </li>
-                <li className={ styles.item }>
-                    <NavLink to="/settings" activeClassName={ styles.active }>
+                <li className={styles.item}>
+                    <NavLink to="/settings" activeClassName={styles.active}>
                         <RiSettings4Line />
                         Settings
                     </NavLink>
