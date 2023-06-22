b039280572ec04e37b31eccd79ca1b9201f5decc
Denis Kulik <kulikdenis1996x@gmail.com>
2023-06-22 20:06:42 +0700

    refactor Users component to use class syntax

--- src/components/Users/index.tsx
+++ src/components/Users/index.tsx
@@ -1,54 +1,62 @@
+import { Component } from 'react';
 import axios from 'axios';
 import styles from './Users.module.scss';
 import { UsersPropsType } from './UsersContainer';
 import userDefault from '../../img/user-default.png';
 
-const Users = (props: UsersPropsType) => {
-    const { usersPage, followUser, unfollowUser, setUsers } = props;
+class Users extends Component<UsersPropsType> {
+    constructor(props: UsersPropsType) {
+        super(props);
 
-    if (!usersPage.users.length) {
         axios.get('https://social-network.samuraijs.com/api/1.0/users')
-             .then((response) => setUsers(response.data.items));
-    }
+             .then((response) => this.props.setUsers(response.data.items));
+    };
 
-    const userItems = usersPage.users.map((user) => (
-        <div key={user.id} className={styles.userItem}>
-            <div className={styles.userSubsBlock}>
-                <img
-                    src={user.photos.large ? user.photos.large : userDefault}
-                    alt={user.name}
-                    width={50}
-                    height={50}
-                />
-                {
-                    user.followed ? (
-                        <button onClick={() => unfollowUser(user.id)}>
-                            Follow
-                        </button>
-                    ) : (
-                        <button onClick={() => followUser(user.id)}>
-                            Unfollow
-                        </button>
-                    )
-                }
-            </div>
-            <div className={styles.userInfo}>
-                <div className={styles.userDetails}>
-                    <h3 className={styles.username}>{user.name}</h3>
-                    <p className={styles.userStatus}>{user.status}</p>
-                </div>
-                <p className={styles.userLocation}>
-                    {`${'user.location.city'}, ${'user.location.country'}`}
-                </p>
-            </div>
-        </div>
-    ));
-
-    return (
+    render = () => (
         <div className={styles.users}>
-            {userItems}
+            {
+                this.props.usersPage.users.map((user) => (
+                    <div key={user.id} className={styles.userItem}>
+                        <div className={styles.userSubsBlock}>
+                            <img
+                                src={user.photos.large ? user.photos.large :
+                                     userDefault}
+                                alt={user.name}
+                                width={50}
+                                height={50}
+                            />
+                            {
+                                user.followed ? (
+                                    <button
+                                        onClick={() =>
+                                            this.props.unfollowUser(user.id)}
+                                    >
+                                        Follow
+                                    </button>
+                                ) : (
+                                    <button
+                                        onClick={() =>
+                                            this.props.followUser(user.id)}
+                                    >
+                                        Unfollow
+                                    </button>
+                                )
+                            }
+                        </div>
+                        <div className={styles.userInfo}>
+                            <div className={styles.userDetails}>
+                                <h3 className={styles.username}>{user.name}</h3>
+                                <p className={styles.userStatus}>{user.status}</p>
+                            </div>
+                            <p className={styles.userLocation}>
+                                {`${'user.location.city'}, ${'user.location.country'}`}
+                            </p>
+                        </div>
+                    </div>
+                ))
+            }
         </div>
     );
-};
+}
 
 export default Users;
