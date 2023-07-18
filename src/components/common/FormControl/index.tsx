b8734de5bb489d4a78b81acd04fa1d24707f8cc8
Denis Kulik <kulikdenis1996x@gmail.com>
2023-07-18 13:56:34 +0700

    add CustomInput for handling form validation

--- src/components/common/FormControl/index.tsx
+++ src/components/common/FormControl/index.tsx
@@ -16,3 +16,19 @@ export const CustomTextarea = ({
         </div>
     );
 };
+
+export const CustomInput = ({
+    input,
+    meta,
+    ...props
+}: WrappedFieldProps) => {
+    const hasError = meta.touched && meta.error;
+
+    return (
+        <div className={`${styles.formControl} ${hasError ?
+            styles.error : ''}`}>
+            <input {...input}{...props} />
+            {hasError && <span>{meta.error}</span>}
+        </div>
+    );
+};
