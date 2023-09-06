65f75e072cdd77797153bb8cc98676398a3068f6
Denis Kulik <kulikdenis1996x@gmail.com>
2023-09-07 00:57:24 +0700

    fix: rememberMe input type

--- src/features/login/ui/login-form/index.tsx
+++ src/features/login/ui/login-form/index.tsx
@@ -19,6 +19,7 @@ const LoginForm = ({ handleSubmit, error, captchaUrl }: LoginFormDomainType) => 
             })}
             <div className={styles.checkbox}>
                 {createField<LoginFormValuesTypeKeys>('rememberMe', [], CustomInput, {
+                    type: 'checkbox',
                     placeholder: 'checkbox',
                 })}
                 <label>Remember me</label>
